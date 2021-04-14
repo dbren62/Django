2ea8cb2ccee7cb5df979fa40203b5b068fa7784d
David Brennan <david_brennan1@baylor.edu>
2021-04-14 15:47:21 -0500

    html stuff

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """ The homepage for learning log. """
+    return render(request, 'MainApp/index.html')
+
