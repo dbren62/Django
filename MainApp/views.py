77bdadbc7fb616f7edd637d5d7b13a009021e674
David Brennan <david_brennan1@baylor.edu>
2021-04-21 15:09:37 -0500

    allow user to add new topic

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Topic, Entry
+from django.shortcuts import render, redirect
+
+from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -22,3 +24,14 @@ def topic(request, topic_id):
     return render(request, 'MainApp/topic.html', context)
 
 def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form':form}
+    return render(request, 'MainApp/new_topic.html', context)
